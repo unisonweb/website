60c799c3f8813ada23dcf853ab4fd91f50d5a158
Simon Højberg <r.hackr@gmail.com>
2021-11-04 16:30:21 -0400

    Use recursive-copy for pre-build step

--- build-docs.md
+++ build-docs.md
@@ -2,4 +2,5 @@
 
 ```ucm
 docs.to-html docs build/docs
+docs.to-html articles build/articles
 ```
--- build.mjs
+++ build.mjs
@@ -1,24 +1,31 @@
+// Pre-build steps
 // 1. Copy build/docs/_sidebar.html to src/_includes/_doc-sidebar-content.njk
 // 2. Copy files from build/docs to src/docs
 // 3. Create layout data file in src/docs
+// 4. Copy files from build/articles to src/articles
+// 5. Create layout data file in src/articles
 
-import fs from "fs";
-import ncp from "ncp";
+import fs from "fs/promises";
+import copy from "recursive-copy";
 
-ncp(
-  "./build/docs/_sidebar.html",
-  "./src/_includes/_doc-sidebar-content.njk",
-  (err) => {
-    if (err) return console.error(err);
+function addLayout(name) {
+  return fs.writeFile(
+    `./src/${name}s/${name}s.json`,
+    `{ "layout": "${name}.njk" }`
+  );
+}
 
-    ncp("./build/docs", "./src/docs", (err) => {
-      if (err) return console.error(err);
-
-      fs.writeFile("./src/docs/docs.json", '{ layout: "doc.njk" }', () => {
-        if (err) return console.error(err);
-
-        console.log("Pre-build done");
-      });
-    });
-  }
-);
+fs.rmdir("./src/docs", { recursive: true, force: true })
+  .then(() => fs.rmdir("./src/articles", { recursive: true, force: true }))
+  .then(() => fs.rm("./src/_includes/_doc-sidebar-content.njk"))
+  .then(() =>
+    copy(
+      "./build/docs/_sidebar.html",
+      "./src/_includes/_doc-sidebar-content.njk"
+    )
+  )
+  .then(() => copy("./build/docs", "./src/docs"))
+  .then(() => addLayout("doc"))
+  .then(() => copy("./build/articles", "./src/articles"))
+  .then(() => addLayout("article"))
+  .catch((ex) => console.error(ex));
