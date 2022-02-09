a13a8bbd8491e2a944fa788b95678c2449cfb504
Simon Højberg <r.hackr@gmail.com>
2022-02-09 11:46:10 -0500

    tooltip handling

--- src/js/site.js
+++ src/js/site.js
@@ -1,10 +1,35 @@
 (() => {
   const all = document.querySelectorAll.bind(document);
+  const one = document.querySelector.bind(document);
 
   [...all(".fold-toggle")].forEach((toggle) => {
     toggle.addEventListener("click", (ev) => {
       const folded = ev.currentTarget.parentNode;
       folded.classList.toggle("is-folded");
+    });
+  });
+
+  [...all(".tooltip-trigger")].forEach((trigger) => {
+    toggle.addEventListener("onMouseOver", (_evt) => {
+      const tooltipContent = one(`#${trigger.dataset.tooltipContentId}`);
+
+      if (tooltipContent) {
+        const tooltip = document.createElement("div");
+        tooltip.classList.add("tooltip", "below", "arrow-middle");
+
+        const bubble = document.createElement("div");
+        bubble.classList.add("tooltip-bubble");
+        bubble.innerHTML = tooltipContent.innerHTML;
+
+        tooltip.appendChild(bubble);
+        trigger.appendChild(tooltip);
+      }
+    });
+
+    toggle.addEventListener("onMouseOut", (_evt) => {
+      [...all(".tooltip")].forEach((tooltip) => {
+        tooltip.remove();
+      });
     });
   });
 })();
