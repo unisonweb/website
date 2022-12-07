24e7773962baec6805120797867498bf4d01c289
Simon Højberg <r.hackr@gmail.com>
2022-12-07 13:40:44 -0500

    Add Rosetta installation instructions

--- src/js/learn.js
+++ src/js/learn.js
@@ -50,56 +50,13 @@
     one("main#learn")?.classList.toggle("show-index");
   });
 
-  // -- Modal -----------------------------------------------------------------
-
   function renderDocFragment(url) {
     fetch(url)
       .then((r) => r.text())
-      .then((html) => renderModal(html));
-  }
-
-  function renderModal(content) {
-    const overlay = document.createElement("div");
-    overlay.classList.add("modal-overlay");
-
-    const modal = document.createElement("dialog");
-    modal.classList.add("modal-dialog");
-    modal.setAttribute("open", true);
-
-    const control = document.createElement("div");
-    control.classList.add("control");
-
-    const icon = document.createElement("div");
-    icon.classList.add("icon");
-    icon.classList.add("icon-x");
-    icon.innerHTML = `
-<svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
-<path d="M2 2L12 12M2 12L12 2" stroke="currentColor" stroke-width="1.5"/>
-</svg>
-    `;
-    control.appendChild(icon);
-    control.addEventListener("click", closeModal);
-
-    const modalContent = document.createElement("div");
-    modalContent.classList.add("modal-content");
-    modalContent.innerHTML = content;
-
-    modal.appendChild(control);
-    modal.appendChild(modalContent);
-    overlay.appendChild(modal);
-
-    overlay.addEventListener("click", (evt) => {
-      if (evt.target == overlay) {
-        closeModal();
-      }
-    });
-
-    $body.prepend(overlay);
-    $body.classList.add("modal-open");
-  }
-
-  function closeModal() {
-    one(".modal-overlay")?.remove();
-    $body.classList.remove("modal-open");
+      .then((html) => {
+        const h = U.div({}, []);
+        h.innerHTML = html;
+        U.renderModal(h);
+      });
   }
 })();
--- src/js/site.js
+++ src/js/site.js
@@ -9,16 +9,17 @@
     });
   });
 
-  [
-    ...all(".folded-summary"),
-    ...all(".folded-details > div .word"),
-  ].forEach((summary) => {
-    summary.addEventListener("click", (ev) => {
-      const folded = ev.currentTarget.closest(".folded");
-      if (!folded) { return; }
-      folded.classList.toggle("is-folded");
-    });
-  });
+  [...all(".folded-summary"), ...all(".folded-details > div .word")].forEach(
+    (summary) => {
+      summary.addEventListener("click", (ev) => {
+        const folded = ev.currentTarget.closest(".folded");
+        if (!folded) {
+          return;
+        }
+        folded.classList.toggle("is-folded");
+      });
+    }
+  );
 
   [...all(".tooltip-trigger")].forEach((trigger) => {
     trigger.addEventListener("mouseenter", (_ev) => {
@@ -71,9 +72,8 @@
     "8a59b2585e45a15548391e3f2a8ab05b"
   );
 
-  const { autocomplete, getAlgoliaResults } = window[
-    "@algolia/autocomplete-js"
-  ];
+  const { autocomplete, getAlgoliaResults } =
+    window["@algolia/autocomplete-js"];
 
   autocomplete({
     debug: true,
@@ -131,9 +131,8 @@
   const $searchButton = one("#autocomplete .toggle");
   const $body = one("body");
 
-  one(".aa-InputWrapperPrefix").innerHTML = $searchButton.querySelector(
-    ".icon"
-  ).outerHTML;
+  one(".aa-InputWrapperPrefix").innerHTML =
+    $searchButton.querySelector(".icon").outerHTML;
 
   $searchButton.addEventListener("click", () => {
     if ($autocomplete.classList.contains("show-search")) {
@@ -159,4 +158,110 @@
     one(".modal-overlay").remove();
     $body.classList.remove("modal-open");
   }
+
+  // API
+  window.U = {
+    renderModal(content) {
+      const overlay = document.createElement("div");
+      overlay.classList.add("modal-overlay");
+
+      const modal = document.createElement("dialog");
+      modal.classList.add("modal-dialog");
+      modal.setAttribute("open", true);
+
+      const control = document.createElement("div");
+      control.classList.add("control");
+
+      const icon = document.createElement("div");
+      icon.classList.add("icon");
+      icon.classList.add("icon-x");
+      icon.innerHTML = `
+  <svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <path d="M2 2L12 12M2 12L12 2" stroke="currentColor" stroke-width="1.5"/>
+  </svg>
+      `;
+      control.appendChild(icon);
+      control.addEventListener("click", U.closeModal);
+
+      const modalContent = document.createElement("div");
+      modalContent.classList.add("modal-content");
+      modalContent.appendChild(content);
+
+      modal.appendChild(control);
+      modal.appendChild(modalContent);
+      overlay.appendChild(modal);
+
+      overlay.addEventListener("click", (evt) => {
+        if (evt.target == overlay) {
+          U.closeModal();
+        }
+      });
+
+      $body.prepend(overlay);
+      $body.classList.add("modal-open");
+    },
+
+    closeModal() {
+      one(".modal-overlay")?.remove();
+      $body.classList.remove("modal-open");
+    },
+
+    node(nodeName, attrs, children) {
+      let n = document.createElement(nodeName);
+
+      for (c of children) {
+        if (typeof c == "string") {
+          n.textContent = c;
+        } else {
+          n.appendChild(c);
+        }
+      }
+
+      for (a in attrs) {
+        if (a === "class_") {
+          n.setAttribute("class", attrs[a]);
+        } else {
+          n.setAttribute(a, attrs[a]);
+        }
+      }
+
+      return n;
+    },
+
+    p(attrs, children) {
+      return U.node("p", attrs, children);
+    },
+
+    h1(attrs, children) {
+      return U.node("h1", attrs, children);
+    },
+
+    div(attrs, children) {
+      return U.node("div", attrs, children);
+    },
+
+    a(attrs, children) {
+      return U.node("a", attrs, children);
+    },
+
+    input(attrs) {
+      return U.node("input", attrs, []);
+    },
+
+    svg(attrs, children) {
+      return U.node(
+        "svg",
+        { ...attrs, xmlns: "http://www.w3.org/2000/svg" },
+        children
+      );
+    },
+
+    path(attrs) {
+      return U.node("path", attrs, []);
+    },
+
+    button(attrs, children) {
+      return U.node("button", attrs, children);
+    },
+  };
 })();
