ac124e01f9a120e663244625ca2b30c3ccde5872
Rebecca Mark <rebecca.mark@unison.cloud>
2024-09-18 17:22:14 -0700

    tweaks to schedule

--- src/_data/conf2024.js
+++ src/_data/conf2024.js
@@ -9,7 +9,7 @@ module.exports = {
     },
     {
       start: "10:05",
-      end: "10:45am",
+      end: "10:40am",
       title: "Full Stack Web Development with Unison, htmx, and Web Components",
       details: `Make modern web applications with Unison using the "WUH?" stack!`,
       speaker: "Tavish Pegram",
@@ -17,7 +17,7 @@ module.exports = {
     },
     {
       start: "10:45",
-      end: "11:25am",
+      end: "11:20am",
       title: "How to Learn a Language (without ever using it)",
       details: `Language parsing is indispensable for software development. It enables various functionalities like syntax checking, code navigation, refactoring, and static analysis. Tree Sitter is a parser generator and incremental parsing library. This talk will cover how parsing a language works and how the Tree Sitter grammar for Unison was written.`,
       speaker: "Kyle Goetz",
@@ -25,7 +25,7 @@ module.exports = {
     },
     {
       start: "11:25",
-      end: "12:05pm",
+      end: "12:00pm",
       title: "Toward a rich and graphical UCM",
       details: `A look to the future where we'll explore the next progression of the Unison Codebase Manager (UCM): a rich, graphical, and keyboard-driven modern UI. How common tasks like type-checking, project and branch navigation, definition management, and yes, editing, could work in a different setting than the terminal.`,
       speaker: "Simon Højberg",
@@ -38,7 +38,7 @@ module.exports = {
     },
     {
       start: "12:20",
-      end: "1:00pm",
+      end: "12:55pm",
       title: "UIs in Unison",
       details: `CLIs and cloud services are right in Unison’s wheelhouse, but often you want to put a GUI in front of the end user. What does that look like in the Unison ecosystem?
 
@@ -48,20 +48,20 @@ module.exports = {
     },
     {
       start: "1:00",
-      end: "1:40pm",
+      end: "1:35pm",
       title: "Boost your command-line applications with potions!",
       details: `This talk presents \`potions\`, a library for parsing command-line arguments: its features, how Unison is leveraged for its implementation (abilities anyone?) and some of the paper cuts that hurt during its development.`,
       speaker: "Eric Torreborre",
       speakerPhotoUrl: "/assets/unison-forall-2024/etorreborre.jpg"
     },
     {
-      start: "12:05",
-      end: "2:10 PM",
+      start: "2:05",
+      end: "2:35 PM",
       title: "30 minute break",
     },
     {
-      start: "2:10",
-      end: "2:50pm",
+      start: "2:35",
+      end: "3:10pm",
       title: "Busy Beaver Problem",
       details: `Turing machines are a universal computing model that are very capable. But even they have their limitations. Some functions are uncomputable.
 
@@ -70,8 +70,8 @@ One class of functions is called the busy beaver problem; for a Turing machine o
       speakerPhotoUrl: "/assets/unison-forall-2024/dvberkel.jpg"
     },
     {
-      start: "2:50",
-      end: "3:35pm",
+      start: "3:15",
+      end: "4:15pm",
       title: "Distributed streaming on Unison Cloud",
       details: `In this talk we’ll look at the implementation of KLogs, a distributed streaming library built entirely on Unison Cloud.
 
@@ -80,29 +80,29 @@ We’ll start our journey in the beautiful landscape of functional programming A
       speakerPhotoUrl: "/assets/unison-forall-2024/fabio.jpg"
     },
     {
-      start: "3:35",
-      end: "3:50 PM",
+      start: "4:20",
+      end: "4:35 PM",
       title: "15 minute break",
     },
     {
-      start: "3:50",
-      end: "4:30pm",
+      start: "4:35",
+      end: "5:10pm",
       title: "Backpropagation in Unison: A Friendly Introduction",
       details: `Backpropagation is the backbone of training neural networks, but how does it actually work? In this session, we’ll break down the essentials of backpropagation in a relaxed, approachable way, explaining the core concepts and the intuition behind the math. We’ll conclude with a dive into how to implement backpropagation in Unison.`,
       speaker: "Gerard Finol",
       speakerPhotoUrl: "/assets/unison-forall-2024/gerardf.jpg"
     },
     {
-      start: "4:30",
-      end: "5:10pm",
+      start: "5:15",
+      end: "5:50pm",
       title: "Fun with Actors in Unison",
       details: `Implementing akka/erlang-style actors/processes in unison. Unison’s “abilities” provide for a very natural and ergonomic experience for using actors. We’ll go through the implementation, live-code some demos, and explore some runtime visualizations.`,
       speaker: "Alvaro Carrasco",
       speakerPhotoUrl: "/assets/unison-forall-2024/alvaro.jpg"
     },
     {
-      start: "5:10",
-      end: "5:20pm",
+      start: "5:55",
+      end: "6:05pm",
       title: "Closing remarks",
       speaker: "Paul Chiusano",
       speakerPhotoUrl: "/assets/unison-forall-2024/pchiusano.jpg"
